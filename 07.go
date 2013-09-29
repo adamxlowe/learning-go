c5b56be74cb5c989faf4fcea6b99a5509f868e59
twmb <travis.bischel@gmail.com>
2013-09-29 13:51:28 -0500

    range: channels never closed

---
+++ 07.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"github.com/twmb/style"
+)
+
+func main() {
+	w := style.NewWriter()
+	fmt.Fprintln(w, "calling RecursiveChannels in main")
+
+	for received := range RecursiveChannels(0) {
+		fmt.Fprintf(w, "received %v from c in main\n", received)
+	}
+}
+
+func RecursiveChannels(recursionlevel int) <-chan int {
+	w := style.NewWriter(style.Bright, style.XTerm256FG(recursionlevel+1))
+
+	returnChan := make(chan int)
+
+	go func() {
+		if recursionlevel < 10 {
+			fmt.Fprintf(w, "recursing and receiving from channel at recursion level %v\n", recursionlevel)
+			for received := range RecursiveChannels(recursionlevel + 1) {
+				fmt.Fprintf(w, "received value %v from channel at recursion level %v, now sending back my level\n", received, recursionlevel)
+			}
+		} else {
+			fmt.Fprintf(w, "end of recursion: sending to channel at recursion level %v\n", recursionlevel)
+		}
+		returnChan <- recursionlevel
+	}()
+
+	return returnChan
+}
